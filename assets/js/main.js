94359301dcba834671b1d3fec07540d22f9a6c38
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-04-12 15:16:52 +0200

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/* Milestone 1
+    Creiamo il nostro array di oggetti che rappresentano ciascun post. Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+    id del post, numero progressivo da 1 a n
+    nome autore,
+    foto autore,
+    data in formato americano (mm-gg-yyyy),
+    testo del post,
+    immagine (non tutti i post devono avere una immagine),
+    numero di likes.
+    Non è necessario creare date casuali 
+*/
