197ccae4ade9f49c5761ffb5409ab7cae40f7f47
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-04-12 18:17:06 +0200

    Add Milestone_3

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,7 @@ const post = [{
     {
         id: 2,
         autore: "Sofia Perlari",
-        foto: "https://picsum.photos/50/50",
+        foto: "https://picsum.photos/80/80",
         data: "12/06/2021",
         testo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui cum, nisi incidunt excepturi non officiis fuga. Molestiae, necessitatibus.",
         img_post: ""
@@ -29,7 +29,7 @@ const post = [{
     {
         id: 3,
         autore: "Ambra Cusin",
-        foto: "https://picsum.photos/50/50",
+        foto: "https://picsum.photos/90/90",
         data: "12/06/2021",
         testo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui cum, nisi incidunt excepturi non officiis fuga. Molestiae, necessitatibus.",
         img_post: "https://picsum.photos/1380/680"
@@ -37,7 +37,7 @@ const post = [{
     {
         id: 4,
         autore: "Gianluca Giovannini",
-        foto: "https://picsum.photos/50/50",
+        foto: "https://picsum.photos/100/100",
         data: "12/06/2021",
         testo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui cum, nisi incidunt excepturi non officiis fuga. Molestiae, necessitatibus.",
         img_post: ""
@@ -45,7 +45,7 @@ const post = [{
     {
         id: 5,
         autore: "Matteo Sperlari",
-        foto: "https://picsum.photos/50/50",
+        foto: "https://picsum.photos/110/110",
         data: "12/06/2021",
         testo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui cum, nisi incidunt excepturi non officiis fuga. Molestiae, necessitatibus.",
         img_post: "https://picsum.photos/1480/780"
@@ -71,7 +71,10 @@ function generatePost(arrayPost, domElement) {
     <div class="cols p-4">
         <div class="card p-3">
             <div class="card_user d-flex align-items-center mt-2">
-                <img class="rounded-circle" src="${element.foto}" alt="img_user">
+                <div class="img_user">
+                    <img class="rounded-circle" src="${element.foto}"   alt="img_user">
+                </div>
+                <!-- /.img_user -->
                 <div class="user ms-3">
                     <div class="name fw-bold">${element.autore}</div>
                     <!-- /.name -->
@@ -95,13 +98,13 @@ function generatePost(arrayPost, domElement) {
             <!-- /.card_body -->
     
             <div class="card_engagement d-flex justify-content-around mb-4">
-                <button class="like">
+                <button id="${element.id}" class="like">
                     <i class="fa-solid fa-thumbs-up"></i>
                     <span>Mi Piace</span>
                 </button>
-                <!-- /.like -->
+                <!-- /#like -->
                 <div class="likers">
-                    Piace a <span><strong>${getRandomInteger()}</strong></span> persone
+                    Piace a <span class="num_${element.id}">${getRandomInteger()}</span> persone
                 </div>
                 <!-- /.likers -->
             </div>
@@ -115,7 +118,10 @@ function generatePost(arrayPost, domElement) {
     <div class="cols p-4">
         <div class="card p-3">
             <div class="card_user d-flex align-items-center mt-2">
-                <img class="rounded-circle" src="${element.foto}" alt="img_user">
+                <div class="img_user">
+                    <img class="rounded-circle" src="${element.foto}"   alt="img_user">
+                </div>
+                <!-- /.img_user -->
                 <div class="user ms-3">
                     <div class="name fw-bold ">${element.autore}</div>
                     <!-- /.name -->
@@ -136,13 +142,13 @@ function generatePost(arrayPost, domElement) {
             <!-- /.card_body -->
     
             <div class="card_engagement d-flex justify-content-around mb-4">
-                <button class="like">
+           <button id="${element.id}" class="like">
                     <i class="fa-solid fa-thumbs-up"></i>
                     <span>Mi Piace</span>
                 </button>
                 <!-- /.like -->
                 <div class="likers">
-                    Piace a <span><strong>${getRandomInteger()}</strong></span> persone
+                    Piace a <span class="num_${element.id}">${getRandomInteger()}</span> persone
                 </div>
                 <!-- /.likers -->
             </div>
@@ -161,6 +167,9 @@ function generatePost(arrayPost, domElement) {
         }
 
     });
+
+
+
 }
 
 generatePost(post, rowElement)
@@ -172,7 +181,33 @@ function getRandomInteger() {
 
 /* Milestone 3
     Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
- */
+*/
 
-// seleziono l'elemento della dom che corrisponde al button "mi piace"
-const likeButtonElement = document.querySelectorAll("button")
+for (let i = 0; i < post.length; i++) {
+    const postSingolo = post[i];
+    //console.log(postSingolo.id)
+
+    let buttonElement = document.getElementById(postSingolo.id)
+        //console.log(buttonElement)
+    let classLike = ".num_" + postSingolo.id
+        //console.log(classLike)
+    let spanElement = document.querySelector(classLike)
+        //console.log(spanElement)
+
+    buttonElement.addEventListener("click", () => {
+        buttonElement.classList.toggle("active")
+
+
+        spanElement.classList.toggle("active")
+
+        const spanInnerHTML = Number(spanElement.innerHTML);
+
+
+        if (spanElement.classList == "num_" + postSingolo.id + " active") {
+            spanElement.innerHTML = Number(spanInnerHTML) + 1;
+        } else {
+            spanElement.innerHTML = Number(spanInnerHTML) - 1;
+        }
+    })
+
+}
